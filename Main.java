2eacd8fa445c4c10c0fb111ad40574b8057888c3
tako2039 <takeo2039@gmail.com>
2022-04-21 18:07:05 -0500

    add feature prompting user

--- Main.java
+++ Main.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // int[] theBinary = {1,0,0,1,0,1,1};
-        // int[] theBinary = {1,0,0,1,0,0,1,0,0,1,0,0};
-        String binary = "1001011";
+
+        // Prompt user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any binary number");
+        String binary = scanner.nextLine();
+
+        // Split String into digits
         String[] arrayOfStringBinary = binary.split("");
+
+        // Define necessary variables
         int exponentialValue = 0;
         int theDecimal = 0;
+
+        // Loop from smallest to largest digits
         for(int i=arrayOfStringBinary.length-1; i >=0; i--) {
             int d = Integer.parseInt(arrayOfStringBinary[i]);
             theDecimal += d * (Math.pow(2.0, exponentialValue));

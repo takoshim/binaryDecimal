fe3e1aa0ff1b73f5ac2d915e0ebd4a4b80df54bb
tako2039 <takeo2039@gmail.com>
2022-04-21 18:34:31 -0500

    create function toDecimal

--- Main.java
+++ Main.java
@@ -6,23 +6,31 @@ public class Main {
         // Prompt user
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter any binary number");
-        String binary = scanner.nextLine();
+        long binary = scanner.nextLong();
 
-        // Split String into digits
-        String[] arrayOfStringBinary = binary.split("");
+        int result = toDecimal(binary);
+        System.out.println("Binary " + binary + " is equal to decimal " + result);
+    }
+    public static int toDecimal(long binary) {
+        // Returns 1 if 1;
+        if (binary == 1) {
+            return 1;
+        }
+        // Changes binary into array of String
+        String binaryInString = Long.toString(binary);
+        String[] arrayOfStringBinary = binaryInString.split("");
 
         // Define necessary variables
         int exponentialValue = 0;
-        int theDecimal = 0;
+        int decimal = 0;
 
         // Loop from smallest to largest digits
-        for(int i=arrayOfStringBinary.length-1; i >=0; i--) {
+        for (int i = arrayOfStringBinary.length - 1; i >= 0; i--) {
             int d = Integer.parseInt(arrayOfStringBinary[i]);
-            theDecimal += d * (Math.pow(2.0, exponentialValue));
+            decimal += d * (Math.pow(2.0, exponentialValue));
             exponentialValue++;
         }
-        System.out.println("The decimal number: " + theDecimal);
+        return decimal;
     }
 }
-
 

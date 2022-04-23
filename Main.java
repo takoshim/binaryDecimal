57c503b1fdaa7f7514745977be1bc432ab66413a
tako2039 <takeo2039@gmail.com>
2022-04-23 17:10:41 -0500

    change return types of two functions

--- Main.java
+++ Main.java
@@ -6,41 +6,39 @@ public class Main {
         // Prompt user
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter any binary number");
-        long binary = scanner.nextLong();
-        int decimalResult = toDecimal(binary);
-        System.out.println("Binary " + binary + " is equal to decimal " + decimalResult);
+        String binaryString = scanner.nextLine();
+        long decimal = changeBinaryInStringToDecimalInLong(binaryString);
+        System.out.println("Binary " + binaryString + " is equal to decimal " + decimal);
 
         // Prompts user
         System.out.println("Enter any decimal number");
-        int decimal = scanner.nextInt();
-        long binaryResult = toBinary(decimal);
+        long decimalLong = scanner.nextLong();
+        String binaryResult = changeDecimalInLongToBinaryInString(decimalLong);
         System.out.println("Decimal " + decimal + " is equal to binary " + binaryResult);
     }
 
     /**
-     * This function returns a decimal number in int type.
-     * @param binary - binary value wanted to be a decimal
-     * @return decimal
+     * This function returns a decimal number in long type.
+     * @param binaryString - binary value needing to be decimal
+     * @return decimal in long
      */
-    public static int toDecimal(long binary) {
+    public static long changeBinaryInStringToDecimalInLong(String binaryString) {
 
         // Returns 1 if 1;
-        if (binary == 1) {
+        if (binaryString == "1") {
             return 1;
         }
-        // Changes long type binary into String
-        String binaryString = Long.toString(binary);
 
         // Splits String type binary into array of String
         String[] binaryArrayString = binaryString.split("");
 
         // Define necessary variables
         int exponentialPower = 0;
-        int decimal = 0;
+        long decimal = 0;
 
         // Loop from small digit to larger digit
         for (int i = binaryArrayString.length - 1; i >= 0; i--) {
-            int d = Integer.parseInt(binaryArrayString[i]);
+            long d = Long.parseLong(binaryArrayString[i]);
 
             // Multiplies each digit by exponential of 2
             decimal += d * (Math.pow(2.0, exponentialPower));
@@ -50,24 +48,24 @@ public class Main {
     }
 
     /**
-     * This function returns a binary number in type long.
-     * @param decimal - decimal value wanted to be a binary
-     * @return binary
+     * This function returns a binary number in type String.
+     * @param decimalLong - decimal value needing to be binary
+     * @return binary in String
      */
-    public static long toBinary(int decimal) {
+    public static String changeDecimalInLongToBinaryInString(long decimalLong) {
 
         // Returns 0 if 0
-        if (decimal ==0) {
-            return 0;
+        if (decimalLong ==0) {
+            return "0";
         }
 
         // Defines necessary variables
-        int quotient = decimal;
+        long quotient = decimalLong;
         String binaryStringReversed = "";
 
         // Concatenates the remainders form binary in reversed order
         while (quotient != 0) {
-            binaryStringReversed = binaryStringReversed.concat(Integer.toString(quotient %2));
+            binaryStringReversed = binaryStringReversed.concat(Long.toString(quotient %2));
             quotient /= 2;
         }
 
@@ -81,7 +79,7 @@ public class Main {
         }
 
         // Parses String type binary into type long and returns
-        return Long.parseLong(binaryString);
+        return binaryString;
     }
 }
 
